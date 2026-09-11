1474fcb6385857ea609489c647cc426a718ed24d
pranjal <kpranjal219@gmail.com>
2026-09-12 01:47:46 +0530

    fix jwks-rsa ESM bug

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  serverExternalPackages: ['firebase-admin'],
+  serverExternalPackages: ['firebase-admin', 'jwks-rsa'],
+  experimental: {
+    serverComponentsExternalPackages: ['firebase-admin', 'jwks-rsa'],
+  },
 };
 
 export default nextConfig;
