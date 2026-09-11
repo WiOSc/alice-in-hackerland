6652ba89216dde33fef116fbb931a658c9d1eab0
pranjal <kpranjal219@gmail.com>
2026-09-12 01:33:24 +0530

    prvt key

--- src/lib/firebase/admin.ts
+++ src/lib/firebase/admin.ts
@@ -17,7 +17,9 @@ function getAdminApp(): App {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+      privateKey: process.env.FIREBASE_PRIVATE_KEY
+        ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n').replace(/^"|"$/g, '')
+        : undefined,
     }),
   });
 }
